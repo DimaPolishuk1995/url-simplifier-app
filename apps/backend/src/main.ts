116465927cc29c00700cff849b8b78d958b6e728
DimaPolishuk1995 <dima.polishuk.1995@gmail.com>
2024-08-16 18:55:58 +0300

    feat(backend): add dynamic port configuration (main)
    - Updated main.ts to use PORT from environment variables.
    - Default to port 4000 if PORT is not set.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(4000);
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
 }
 bootstrap();
