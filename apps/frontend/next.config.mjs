5d62baae3fbdaeaff2c27a448d1bb962df525653
DimaPolishuk1995 <dima.polishuk.1995@gmail.com>
2024-08-16 13:28:31 +0300

    feat(frontend): update layout, styles, and add new dependencies (develop)
    - Updated layout.tsx to simplify metadata and remove redundant Head tags.
    - Modified global CSS background colors for light and dark modes.
    - Added react-copy-to-clipboard and its types to package.json.
    - Updated next.config.mjs to add a new redirect rule for short URLs.

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         source: '/api/:path*',
         destination: 'http://localhost:4000/:path*',
       },
+      {
+        source: '/:shortUrl',
+        destination: 'http://localhost:4000/url/:shortUrl',
+      },
     ];
   },
 };
--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,38 +1,30 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Head from "next/head";
-
 import Providers from "./Providers";
-
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "URL Simplifier",
-  description: "Fast and efficient link shortening service. Simplify your URLs with ease.",
+  description:
+    "Fast and efficient link shortening service. Simplify your URLs with ease.",
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="robots" content="index, follow" />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="en_US" />
-        <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline';" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      </Head>
       <body className={inter.className}>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
